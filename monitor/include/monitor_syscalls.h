b9e6d4f26b4608904cab51e046875fa933d5af54
bataille16 <bataille16@gmail.com>
2013-02-19 21:42:22 -0500

    declaring extern variables

--- monitor/include/monitor_syscalls.h
+++ monitor/include/monitor_syscalls.h
@@ -12,7 +12,20 @@ Header files describing the overriden system calls
 #ifndef MONITOR_SYSCALLS_H
 #define MONITOR_SYSCALLS_H
 
-// sys_open call overide
-int our_sys_open(void);
+
+
+
+extern int PID;
+extern void **sys_call_table; // pointer to system call table on machine
+// change memory page read/write options
+
+// save/set and reset sys calls
+void save_originals(void);
+void load_monitor_syscalls(void);
+void cleanup(void);
+
+// open (0x5)
+//asmlinkage int (*original_open) (const char *, int, int);
+asmlinkage int monitor_open(const char *, int, int);
 
 #endif //MONITOR_SYSCALLS_H
